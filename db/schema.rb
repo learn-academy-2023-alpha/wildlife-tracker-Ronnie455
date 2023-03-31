65dcfb480ebeb24d790a3bc5fe1405019562a478
Ronnie455 <ronniemaynard4@gmail.com>
2023-03-31 10:26:15 -0700

    completed story 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_220300) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_162128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "common_name"
     t.string "scientific_bionomial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "animal_di"
+    t.float "latitude"
+    t.float "longitude"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
